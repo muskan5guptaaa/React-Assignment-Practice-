618565dca293d7f046fb622e9348507e684a17c8
muskan5guptaaa <muskan5gupta123@gmail.com>
2025-01-14 23:22:21 +0530

    chnges

--- checkbox/src/checkbox.jsx
+++ checkbox/src/checkbox.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 
 function LikeCheckbox() {
-  // State variable to track if the checkbox is liked
   const [liked, setLiked] = useState(false);
 
   // Function to handle checkbox change
   const handleCheckboxChange = (event) => {
-    setLiked(event.target.checked); // Update state with checkbox value
+    setLiked(event.target.checked);
   };
 
   return (
