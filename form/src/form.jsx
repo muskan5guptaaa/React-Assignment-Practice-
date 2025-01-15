62b5c0dd3af23e3b16dd6d40adc27c12868e9b43
muskan5guptaaa <muskan5gupta123@gmail.com>
2025-01-15 20:43:25 +0530

    create form

---
+++ form/src/form.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const form=()=>{
+  const[userName,setuserName]=useState
+}
+
+return(
+    <div>
+        <form onSubmit={}>
+
+       
+        <input>
+        </input>
+        </form>
+    </div>
+)
+export default form
